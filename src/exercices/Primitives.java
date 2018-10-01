33590d7cccf5538f69ecd05c50f4d8acad653c1d
Mathilde Loiselet <mathilde.loiselet@gmail.com>
2018-10-01 14:55:53 +0200

    Exercice Java training 1

--- src/exercices/Primitives.java
+++ src/exercices/Primitives.java
@@ -2,19 +2,28 @@ package exercices;
 
 public class Primitives {
 
-    // TODO 1 : Change the method 'integer' to return the expected result : int 42
-    public static int integer() {
-        return 0;
+    public static void main(String[] args){
+        
     }
 
-    // TODO 2 : Create a method 'str', result expected : String "Hello"
+    public static int integer() {
+        return 42;
+    }
 
+    public static String str(){
+        return "Hello";
+    }
 
-    // TODO 3 : Create a method 'real', result expected : double 3.14
+    public static double real(){
+        return 3.14;
+    }
 
+    public static boolean bool(){
+        boolean bool = true;
+        return (!bool);
+    }
 
-    // TODO 4 : Create a method 'bool', result expected : boolean false
+    public static void nothing(){
 
-
-    // TODO 5 : Create a method 'nothing', no result expected
+    }
 }
